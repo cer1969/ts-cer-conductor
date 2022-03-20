5817bb602699f5d81dbfdc829c0228edc4035ecf
cer1969 <cer1969@gmail.com>
2022-03-20 00:20:34 -0300

    Cambia nombre lib y forma de importar

--- index.ts
+++
@@ -1,8 +0,0 @@
-// CRISTIAN ECHEVERRÍA RABÍ
-
-export * from "./src/checker"
-export * from "./src/constants"
-export * from "./src/category"
-export * from "./src/conductor"
-export * from "./src/currentcalc"
-export * from "./src/operatingtable"
